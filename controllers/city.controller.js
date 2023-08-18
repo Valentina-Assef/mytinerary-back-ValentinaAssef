692ce5bb4389b85bbec94ffc62ad4318c28db97f
Valentina-Assef <valenassef@gmail.com>
2023-08-18 01:20:59 -0300

    Database created

--- controllers/city.controller.js
+++ controllers/city.controller.js
@@ -1,21 +1,33 @@
 import City from "../models/City.js";
 
 const controller = {
-    getCities: (req, res) => {
-        console.log("Se ejecuto controlador de getCities")
-
-        res.send("getCities")
+    getCities: async (req, res) => {
+        try {
+            const cities = await City.find()
+            return res.status(200).json({
+                success: true,
+                cities
+            })
+        } catch (error) {
+            return res.status(500).json({
+                success: false,
+                message: "Failed to get information"
+            })
+        }
     },
     createCity: async (req, res) => {
-        const newCity = await City.create({ 
-            "name": "Paris",
-            "country": "France",
-            "image": "/public/img/paris.jpg"
-        })
-        return res.status(201).json({
-            success: true,
-            message: "City created"
-        })
+        try {
+            const newCity = await City.create(req.body)
+            return res.status(201).json({
+                success: true,
+                message: "City created"
+            })
+        } catch (error) {
+            return res.status(500).json({
+                success: false,
+                message: "Failed to create"
+            })
+        }
     },
     postCity: () => {},
     deleteCity: () => {},
