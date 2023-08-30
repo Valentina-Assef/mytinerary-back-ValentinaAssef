432961a9daef83a961e52ad937ae4b378ae626dd
Valentina-Assef <valenassef@gmail.com>
2023-08-30 18:44:17 -0300

    Update Itinerary model

--- models/Itinerary.js
+++ models/Itinerary.js
@@ -2,17 +2,18 @@ import { Schema, model, Types } from "mongoose";
 
 const collection = "itineraries";
 const schema = new Schema({
-    itinerary_name: { type: String, required: true },
-    user_img: { type: String, required: true },
-    user_name: { type: String, required: true },
+    name: { type: String, required: true },
     price: { type: Number, required: true },
     duration: { type: String, required: true },
     hashtag_1: { type: String, required: true },
     hashtag_2: { type: String, required: true },
     hashtag_3: { type: String, required: true },
+    user: {
+        user_name: { type: String, required: true },
+        user_img: { type: String, required: true },
+    },
     comment: { type: String, required: true },
-    city: { type: Types.ObjectId, ref: "cities" },
-    user: { type: Types.ObjectId, ref: "users" }
+    city: { type: Types.ObjectId, ref: "cities" }
 },{
     timestamps: true
 });
