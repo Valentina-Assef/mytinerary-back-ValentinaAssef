09eba0df6dee249abceee966e13b2593697b14fa
Valentina-Assef <valenassef@gmail.com>
2023-08-31 02:31:51 -0300

    update itinerary controller & router getByCity

--- router/itinerary.router.js
+++ router/itinerary.router.js
@@ -2,10 +2,11 @@ import express from "express";
 import itineraryController from "../controllers/itinerary.controller.js"
 
 const router = express.Router();
-const {getItineraries, getItineraryById, createItinerary, updateItinerary, deleteItinerary } = itineraryController;
+const {getItineraries, getItineraryById, getItinerariesByCity, createItinerary, updateItinerary, deleteItinerary } = itineraryController;
 
 router.get('/', getItineraries);
 router.get('/:id', getItineraryById);
+router.get('/city/:cityId', getItinerariesByCity);
 router.post('/', createItinerary);
 router.put('/:id', updateItinerary);
 router.delete('/:id', deleteItinerary);
