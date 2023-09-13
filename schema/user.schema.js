76130bc2cf1a5bed6ccef5be334376e255107176
Valentina-Assef <valenassef@gmail.com>
2023-09-13 02:47:11 -0300

    User schema update

--- schema/user.schema.js
+++ schema/user.schema.js
@@ -4,15 +4,15 @@ export const createUserSchema = Joi.object ({
   name: Joi.string().required()
     .min(2)
     .max(50),
-  lastName: Joi.string().required()
+  lastName: Joi.string()
     .min(2)
     .max(50),
   email: Joi.string().required()
     .email({
       minDomainSegments: 2
     }),
-  country: Joi.string().required(),
-  photo: Joi.string().required()
+  country: Joi.string(),
+  photo: Joi.string()
     .uri(),
   password: Joi.string().required()
     .min(8)
